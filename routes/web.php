9e1f8109bdd7f4bc6138a28e1f4cd4b717c5ae48
Yuma Akhunza Kausar Putra <yuma.akhunza@gmail.com>
2025-02-25 04:05:39 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,63 +1,63 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController;
-use App\Http\Controllers\PhotoController;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\WelcomeController;
+// use App\Http\Controllers\PhotoController;
 
-//----Basic Routing----//
-// Route::get('/hello', function () { 
-//     return 'Hello World'; 
-// });
+// //----Basic Routing----//
+// // Route::get('/hello', function () { 
+// //     return 'Hello World'; 
+// // });
 
-// Route::get('/world', function () { 
-//     return 'World'; 
-// }); 
+// // Route::get('/world', function () { 
+// //     return 'World'; 
+// // }); 
 
-// Route::get('/world', function () { 
-//     return 'Selamat Datang'; 
-// }); 
+// // Route::get('/world', function () { 
+// //     return 'Selamat Datang'; 
+// // }); 
 
-// Route::get('/about', function () { 
-//     return '2341720259 Yuma Akhunza Kausar Putra'; 
-// }); 
+// // Route::get('/about', function () { 
+// //     return '2341720259 Yuma Akhunza Kausar Putra'; 
+// // }); 
 
-//----Route Parameters----//
-// Route::get('/user/{name}', function ($name) { 
-//     return 'Nama saya '.$name; 
-//}); 
+// //----Route Parameters----//
+// // Route::get('/user/{name}', function ($name) { 
+// //     return 'Nama saya '.$name; 
+// //}); 
 
-// Route::get('/posts/{post}/comments/{comment}', function 
-// ($postId, $commentId) { 
-//      return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
-// });
+// // Route::get('/posts/{post}/comments/{comment}', function 
+// // ($postId, $commentId) { 
+// //      return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+// // });
 
-// Route::get('/articles/{id}', function ($articlesId) { 
-//      return 'Halaman Artikel Ke- ' .$articlesId; 
-// });
+// // Route::get('/articles/{id}', function ($articlesId) { 
+// //      return 'Halaman Artikel Ke- ' .$articlesId; 
+// // });
 
-// Route::get('/user/{name?}', function ($name='John') {
-//     return 'Nama saya '.$name;
-// });
+// // Route::get('/user/{name?}', function ($name='John') {
+// //     return 'Nama saya '.$name;
+// // });
 
-//----Route Name----//
-// Route::get('/user/profile', function () {
-//    //
-// })->name('profile');
+// //----Route Name----//
+// // Route::get('/user/profile', function () {
+// //    //
+// // })->name('profile');
 
-//----Membuat Controller----//
-// Route::get('/hello',[WelcomeController::class,'hello']);
-// Route::resource('photos', PhotoController::class);
-// Route::resource('photos', PhotoController::class)->only([ 
-//     'index', 'show' 
-//     ]);
-// Route::resource('photos', PhotoController::class)->except([ 
-//     'create', 'store', 'update', 'destroy' 
-//     ]); 
+// //----Membuat Controller----//
+// // Route::get('/hello',[WelcomeController::class,'hello']);
+// // Route::resource('photos', PhotoController::class);
+// // Route::resource('photos', PhotoController::class)->only([ 
+// //     'index', 'show' 
+// //     ]);
+// // Route::resource('photos', PhotoController::class)->except([ 
+// //     'create', 'store', 'update', 'destroy' 
+// //     ]); 
 
-//----View----//
-// Route::get('/greeting', function () { 
-//     return view('blog.hello', ['name' => 'Putra']); 
-//     }); 
+// //----View----//
+// // Route::get('/greeting', function () { 
+// //     return view('blog.hello', ['name' => 'Putra']); 
+// //     }); 
 
-Route::get('/greeting', [WelcomeController::class, 
-'greeting']); 
+// Route::get('/greeting', [WelcomeController::class, 
+// 'greeting']); 
